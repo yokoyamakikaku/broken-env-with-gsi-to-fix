60eaaa300fab7fe1b1ca2fb6fb80bfa2625f5980
YOKOYAMA Taku <seekseep@gmail.com>
2023-06-26 20:30:29 +0900

    create next app and amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "brokenenvwithgsibc0e92fd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
